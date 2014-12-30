b758d38e1a1f294dd2a5223afd388b3241c99dfb
unknown <devgujar@gmail.com>
2014-12-30 10:21:36 +0530

    First commit:Initial Draft

---
+++ myproject/src/LibraryPackage/Project_Interface.java
@@ -0,0 +1,12 @@
+package LibraryPackage;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import jxl.read.biff.BiffException;
+
+public interface Project_Interface {	 
+	 public void LogIn_As_User(String UserName, String Password); 
+	 // Function to get data from XML
+
+}
---
+++ myproject/src/TestSuite/TC005_Check_Post_Updating_With_Media/TC005_Check_Post_Updating_With_Media.java
@@ -0,0 +1,19 @@
+package TestSuite.TC005_Check_Post_Updating_With_Media;
+import LibraryPackage.*;
+
+import org.junit.Test;
+import java.io.IOException;
+import jxl.read.biff.BiffException;
+
+public class TC005_Check_Post_Updating_With_Media {
+
+	@Test
+	public void TC005_Check_Post_Updating_With_Media() throws BiffException, IOException {
+		
+		Project_Interface_Definations library = new Project_Interface_Definations();
+		library.start_setup();
+		
+		
+	}
+
+}
